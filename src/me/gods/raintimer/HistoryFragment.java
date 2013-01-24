35b43b8b80b26900717ca49739f9ffe32ab5ec8e
Yicheng Liu <liuchen250.student@gmail.com>
2013-01-24 08:57:26 +0800

    date picker remenber

--- src/me/gods/raintimer/HistoryFragment.java
+++ src/me/gods/raintimer/HistoryFragment.java
@@ -9,6 +9,7 @@ import android.app.Dialog;
 import android.app.DialogFragment;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,6 +25,9 @@ public class HistoryFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_history, container, false);
+
+        startDate = null;
+        endDate = null;
 
         startDateView = (TextView)v.findViewById(R.id.start_date_picker);
         startDateView.setOnClickListener(new View.OnClickListener() {
@@ -55,18 +59,27 @@ public class HistoryFragment extends Fragment {
         @Override
         public Dialog onCreateDialog(Bundle savedInstanceState) {
             // Use the current date as the default date in the picker
-            final Calendar c = Calendar.getInstance();
-            int year = c.get(Calendar.YEAR);
-            int month = c.get(Calendar.MONTH);
-            int day = c.get(Calendar.DAY_OF_MONTH);
+            int year, month, day;
 
-            // Create a new instance of DatePickerDialog and return it
+            if (this.getTag().equals("Start") && startDate == null || this.getTag().equals("End") && endDate == null) {
+                final Calendar c = Calendar.getInstance();
+                year = c.get(Calendar.YEAR);
+                month = c.get(Calendar.MONTH);
+                day = c.get(Calendar.DAY_OF_MONTH);
+            } else {
+                Date dateToShow = this.getTag().equals("Start") ? startDate : endDate;
+
+                year = dateToShow.getYear();
+                month = dateToShow.getMonth();
+                day = dateToShow.getDate();
+            }
+
             return new DatePickerDialog(getActivity(), this, year, month, day);
         }
 
         @Override
         public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-            if (this.getTag() == "Start") {
+            if (this.getTag().equals("Start")) {
                 startDate = new Date(year, monthOfYear, dayOfMonth);
                 startDateView.setText(String.format("%s-%02d-%04d", new DateFormatSymbols().getMonths()[monthOfYear].substring(0,3), dayOfMonth, year));
             } else {
